80b6b0311dcc3b60a0550d4f1a1ff28c95faa43d
KevenBlue <myloveyim@qq.com>
2026-05-27 10:37:43 +0800

    新增标准设备类，用于模拟器组装信号

---
+++ src/main/java/com/wantllife/domain/vo/StandardDevice.java
@@ -0,0 +1,32 @@
+package com.wantllife.domain.vo;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * 设备
+ *
+ * @author KevenPotter
+ * @date 2026-04-28 09:54:33
+ */
+@Data
+@Accessors(chain = true)
+public class StandardDevice {
+
+    /*设备编号*/
+    private String deviceId;
+    /*设备类型*/
+    private Integer deviceType;
+    /*充电枪数量*/
+    private Integer gunNum;
+    /*通信协议版本*/
+    private String protocolVersion;
+    /*程序版本*/
+    private String programVersion;
+    /*网络连接类型*/
+    private Integer networkLinkType;
+    /*sim卡*/
+    private String simNo;
+    /*运营商*/
+    private Integer carrier;
+}
